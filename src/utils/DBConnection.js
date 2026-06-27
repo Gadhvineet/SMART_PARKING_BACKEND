72fdd04e71ad407d12795ace968994c403f35604
Vineet Gadhvi <gadhvineet1326@gmail.com>
2026-06-27 23:51:00 +0530

    mongo online connected

--- src/utils/DBConnection.js
+++ src/utils/DBConnection.js
@@ -1,14 +1,31 @@
-const mongoose = require ('mongoose');
-require('dotenv').config();
+// const mongoose = require ('mongoose');
+// require('dotenv').config();
 
-const DBConnection = () => {
-    mongoose.connect(process.env.MONGO_URI)
-    .then(() => {       
-        console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
+// const DBConnection = () => {
+//     mongoose.connect(process.env.MONGO_URI)
+//     .then(() => {       
+//         console.log('Connected to MongoDB');
+//     })
+//     .catch((err) => {
+//         console.error('Error connecting to MongoDB:', err);
+//     });
+// };
+
+// module.exports = DBConnection;
+const mongoose = require("mongoose");
+
+const DBConnection = async () => {
+  try {
+    console.log("Connecting to:", process.env.MONGO_URI);
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
     });
+
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = DBConnection;
